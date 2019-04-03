a0615577be54482e9d3e82492f46cdfc1a2ea61a
Joey Zhou <joey.zhou@arterys.com>
2019-04-03 14:37:55 +0000

    Initialise docker and web client

---
+++ client/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash                                                                    
+npm install                                           
+ng serve --host 0.0.0.0 --port 4200 >> /ng_server.log 2>&1
